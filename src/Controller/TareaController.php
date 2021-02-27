662cc282b07aa71e467dd15fffa8937cf4bf3823
tostaus <tostaus@hotmail.com>
2021-02-27 10:18:26 +0100

    commit

--- src/Controller/TareaController.php
+++ src/Controller/TareaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TareaRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,11 +10,40 @@ use Symfony\Component\Routing\Annotation\Route;
 class TareaController extends AbstractController
 {
     /**
-     * @Route("/tarea", name="tarea")
+     * @Route("/", name="app_listado_tarea")
      */
-    public function index(): Response
+    public function listado(TareaRepository $tareaRepository)
     {
-        return $this->render('tarea/index.html.twig', [
+        $tareas = $tareaRepository->findAll();
+        return $this->render('tarea/listado.html.twig', [
+            'tareas' => $tareas,
+        ]);
+    }
+
+    /**
+     * @Route("/tarea/crear", name="app_crear_tarea")
+     */
+    public function crear()
+    {
+        return $this->render('tarea/crear.html.twig', [
+            'controller_name' => 'TareaController',
+        ]);
+    }
+     /**
+     * @Route("/tarea/editar/{id}", name="app_editar_tarea")
+     */
+    public function editar(int $id)
+    {
+        return $this->render('tarea/editar.html.twig', [
+            'controller_name' => 'TareaController',
+        ]);
+    }
+     /**
+     * @Route("/tarea/eliminar/{id}", name="app_eliminar_tarea")
+     */
+    public function eliminar(int $id)
+    {
+        return $this->render('tarea/eliminar.html.twig', [
             'controller_name' => 'TareaController',
         ]);
     }
