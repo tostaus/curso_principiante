2696f12979b24371917377e90ff780ea9324897c
tostaus <tostaus@hotmail.com>
2021-02-27 12:25:45 +0100

    commit

--- src/Controller/TareaController.php
+++ src/Controller/TareaController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Tarea;
 use App\Repository\TareaRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -23,21 +26,66 @@ class TareaController extends AbstractController
     /**
      * @Route("/tarea/crear", name="app_crear_tarea")
      */
-    public function crear()
+    public function crear(Request $request, EntityManagerInterface $em): Response
     {
+
+        $descripcion = $request->request->get('descripcion', null);
+        $tarea = new Tarea();
+        if (null !== $descripcion) {
+            if (!empty($descripcion)) {
+                $em = $this->getDoctrine()->getManager();
+                $tarea->setDescripcion($descripcion);
+                $em->persist($tarea);
+                $em->flush();
+                $this->addFlash(
+                    'success',
+                    'Tarea creada correctamente!'
+                );
+                return $this->redirectToRoute('app_listado_tarea');
+            } else {
+                $this->addFlash(
+                    'warning',
+                    'El campo "Descripción es obligatorio"'
+                );
+            }
+        }
         return $this->render('tarea/crear.html.twig', [
-            'controller_name' => 'TareaController',
+            "tarea" => $tarea,
         ]);
     }
      /**
      * @Route("/tarea/editar/{id}", name="app_editar_tarea")
      */
-    public function editar(int $id)
+    public function editar(int $id, TareaRepository $tareaRepository, Request $request, EntityManagerInterface $em): Response
     {
+        $tarea = $tareaRepository->find($id);
+        //$tarea = $tareaRepository->findOneById($id);
+        if (null === $tarea) {
+            throw  $this->createNotFoundException();
+        }
+        $descripcion = $request->request->get('descripcion', null);
+        if (null !== $descripcion) {
+            if (!empty($descripcion)) {
+                $em = $this->getDoctrine()->getManager();
+                $tarea->setDescripcion($descripcion);
+                $em->flush();
+                $this->addFlash(
+                    'success',
+                    'Tarea editada correctamente!'
+                );
+                return $this->redirectToRoute('app_listado_tarea');
+            } else {
+                $this->addFlash(
+                    'warning',
+                    'El campo "Descripción" es obligatorio'
+                );
+            }
+        }
         return $this->render('tarea/editar.html.twig', [
-            'controller_name' => 'TareaController',
+            "tarea" => $tarea,
         ]);
     }
+
      /**
      * @Route("/tarea/eliminar/{id}", name="app_eliminar_tarea")
      */
